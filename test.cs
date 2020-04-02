639eb21ee37131b0c12138a991e43c02dc5c8525
严礼栓 <yanlishuan@palmax.cn>
2020-04-02 18:26:08 +0800

    1.修改

--- test.cs
+++ test.cs
@@ -1,18 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class test : MonoBehaviour
+namespace pomelo.testpackage{
+	public class test : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public const int aa=3;
 }
+}
+
